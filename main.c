7cfa659a32d50aec5b88f595689561237524132f
LuisApolonio0 <contatoluisfelipeapolonio@gmail.com>
2025-11-11 23:22:13 -0300

    estrutura de repetição para perguntar se o usuario deseja cadastrar outro produto.

--- main.c
+++ main.c
@@ -12,14 +12,26 @@ typedef struct
 int main()
 {
     ProdutoEstoque produto;
+    char linha[50];
+    char opcao;
 
+    do{
     printf("========Cadastro de remedios========\n\n");
+
     printf("Digite o nome do produto:\n");
     fgets(produto.nome, 20, stdin);
-    printf("Digite as informaçoes do produto na exata ordem: Codigo, valor(ex.:12.99), quantidade):\n");
-    scanf("%d,%f,%d", &produto.codigo, &produto.preco, &produto.quantidade);
 
-    printf("Código: %d, Valor: %.2f, Quantidade: %d, Nome: %s.\n", produto.codigo, produto.preco, produto.quantidade, produto.nome);
+    printf("Digite as informacoes do produto na exata ordem: Codigo, valor(ex.:12.99), quantidade):\n");
+    fgets(linha, 50, stdin);
+    sscanf(linha, "%d, %f, %d", &produto.codigo, &produto.preco, &produto.quantidade);
+
+    printf("Codigo: %d, Valor: %.2f, Quantidade: %d, Nome: %s.\n", produto.codigo, produto.preco, produto.quantidade, produto.nome);
+
+    printf("Deseja continuar?(Use S para Sim ou N para Não)\n");
+    scanf("%c", &opcao);
+    getchar();
+
+    }while(opcao == 's' || opcao == 'S');
 
     return 0;
 }
